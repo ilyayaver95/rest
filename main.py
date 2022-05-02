e15b5255e4823b817d98d81fb853a7fca7f3a1c9
Ilyay <ilya.yaver@gmail.com>
2022-05-02 13:53:24 +0300

    .

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# 1. crawl every restaurant in rest.co.il
+# 2. extract data into DF
+# 3. create new feature "score" based of feature engeeniring
+# .
