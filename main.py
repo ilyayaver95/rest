a7023f682f760fbae6d6bae15a44291787da05a4
ozehavi <ozehavi@gmail.com>
2022-05-03 23:02:26 +0300

    change get_page to get_page_soup

--- main.py
+++ main.py
@@ -14,37 +14,40 @@ Those function will be called from 'extract_page_attributes' function
 final_box: func(stars + num_feedbacks)....
 '''
 
-def get_page(url):
+def get_page_soup(url):
     try:
         agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
         page = requests.get(url, verify=False, headers=agent)
-        return page
+        soup = BeautifulSoup(page.content, 'html.parser')
+        return soup
     except Exception as e:
         print("Could not get page {}: \n {}".format(url, e))
     return None
 
-def extract_page_attributes(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    feature_column = soup.find_all("div", attrs={"class":"feature-column"})
+def get_page_attributes(page): 
+    
+    return None
+
+def extract_page_attributes(page):
+    feature_column = page.find_all("div", attrs={"class":"feature-column"})
     for col in feature_column:
         pageid = col.attrs["data-customer"]
         print("page id "+pageid)
-        feature_page = get_page("https://www.rest.co.il/rest/" + pageid)
-        feature_soup = BeautifulSoup(feature_page.content, 'html.parser')
+        feature_page = get_page_soup("https://www.rest.co.il/rest/" + pageid)
 
         # need to extract features
 
 def get_body_for_pages(num):
-    page = get_page("https://www.rest.co.il/restaurants/israel")
-    data.append(extract_page_attributes(page.content))
+    page = get_page_soup("https://www.rest.co.il/restaurants/israel")
+    data.append(extract_page_attributes(page))
     if(num == 1):
         return 
     for i in range(1,num):
         print("page ", i)
-        page = get_page("https://www.rest.co.il/restaurants/israel/page-{}/".format(i))
+        page = get_page_soup("https://www.rest.co.il/restaurants/israel/page-{}/".format(i))
         if(page is None):
             break
-        extract_page_attributes(page.content)
+        extract_page_attributes(page)
 
 
 body = get_body_for_pages(2)
