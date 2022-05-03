3fc99376a22d6e95ae52d5fbf8a46015a3e02ab1
ozehavi <ozehavi@gmail.com>
2022-05-03 19:14:52 +0300

    get feature column

--- main.py
+++ main.py
@@ -2,14 +2,38 @@ from bs4 import BeautifulSoup
 import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+data = []
 
-url = r"https://www.rest.co.il/restaurants/israel"
-agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-page = requests.get(url, verify=False, headers=agent)
+def get_page(url):
+    try:
+        agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+        page = requests.get(url, verify=False, headers=agent)
+        return page
+    except Exception as e:
+        print("Could not get page {}: \n {}".format(url, e))
+    return None
 
-rest_soup = BeautifulSoup(page.content, 'html.parser')
+def extract_page_attributes(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    feature_column = soup.find_all("div", attrs={"class":"feature-column"})
+    for col in feature_column:
+        pageid = col.attrs["data-customer"]
+        feature_page = get_page("https://www.rest.co.il/rest/" + pageid)
+        feature_soup = soup = BeautifulSoup(feature_page.content, 'html.parser')
+        # need to extract features
 
-#print(rest_soup.prettify())
+def get_body_for_pages(num):
+    page = get_page("https://www.rest.co.il/restaurants/israel")
+    data.append(extract_page_attributes(page.content))
+    if(num == 1):
+        return 
+    for i in range(1,num):
+        print("page ", i)
+        page = get_page("https://www.rest.co.il/restaurants/israel/page-{}/".format(i))
+        if(page is None):
+            break
+        extract_page_attributes(page.content)
 
-text = rest_soup.find_all("div", attrs={"class":"feature-column"})
-print(len(text))
+
+body = get_body_for_pages(2)
+
