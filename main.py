9b817ea9edb2423f89ca3321d8057dfc6c9680e4
Ilyay <ilya.yaver@gmail.com>
2022-05-19 20:37:32 +0300

    before merge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import math
 from turtle import pd
 import pandas as pd
+import numpy as np
 from attr import attributes
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -10,16 +12,19 @@ from geopy.geocoders import Nominatim
 import geopy
 from pathlib import Path  
 from selenium import webdriver
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from scipy import stats
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 data = []
 
 '''
-Empty attributes need to be set to 0
-
-final_box: func(stars + num_feedbacks)....
-'''
+# 
+# final_box: func(stars + num_feedbacks)....
+# '''
 
 def get_page_soup(url):
     try:
@@ -58,6 +63,18 @@ def get_page_attributes(body):
     attributes = [attr.text for attr in attributes_body]
     return attributes
 
+def get_type(feature_page):
+    """
+    gets the type of restaurant: Italian/
+    :param body:
+    :return:
+    """
+    try:
+        name = feature_page.find("h6")  # , attrs={"class": "main_banner_content"}
+        return name.text.split(',')[0].strip()  # take the name "name, location" and leave only the name
+    except Exception as e:
+        print("[get_type] error: ", e)
+
 def get_number_of_reviews(body):
     try:
         reviews_body = body.find('div', {'class':'raviews_box_item'})
@@ -93,7 +110,6 @@ def get_stars(feature_page):
         stars = feature_page.find("div", attrs={"class":"reviews_wrap"}).find("span")  # , attrs={"class": "main_banner_content"}
     else:
         stars = None
-    # print(stars)
     if(stars):
         return stars.text
     else:
@@ -101,7 +117,6 @@ def get_stars(feature_page):
 
 def get_geolocation(feature_page):
     """
-
     :param feature_page:
     :return: geolocation of rest
     """
@@ -115,26 +130,35 @@ def get_geolocation(feature_page):
     # return address.text  # take the address
 
 def save_df_to_csv(df):
+    """
+    cerates csv based on the DF.
+    """
     folder_name = "Resturants Output"
     if(not os.path.exists(folder_name)):
         os.mkdir(folder_name)
     file_name = "Rest df {}.csv".format(datetime.now().strftime("%d.%b.%Y %H-%M-%S"))
-    filepath = Path("{}/{}".format(folder_name, file_name))  
-    filepath.parent.mkdir(parents=True, exist_ok=True)  
+    filepath = Path("{}/{}".format(folder_name, file_name))
+    filepath.parent.mkdir(parents=True, exist_ok=True)
     df.to_csv(filepath, encoding = 'utf-8-sig', index=False)
-    print("Your df is saved !") 
+    print("Your df is saved !")
 
 def extract_page_attributes(page):
+    """
+
+    :param page:
+    :return: list of JSON of restaurants
+    """
     feature_column = page.find_all("div", attrs={"class":"feature-column"})
     print("feature num ", len(feature_column))
-    data = []
-    for col in feature_column:
+    data_local = []  # list of restaurants on specific page
+    for col in feature_column:  # runs on every restaurant page
         try:
             pageid = col.attrs["data-customer"]
             print("page id "+pageid)
             url = "https://www.rest.co.il/rest/" + pageid
             feature_body = get_page_soup(url)
             name = get_name(feature_body)
+            type = get_type(feature_body)
             page_attributes = get_page_attributes_sel(url, feature_body)
             num_of_reviews = get_number_of_reviews(feature_body)
             stars = get_stars(feature_body)
@@ -142,21 +166,22 @@ def extract_page_attributes(page):
             resturant = {
                 'id'             : pageid,
                 'name'           : name,
+                'type'           : type,
                 'stars'          : stars,
-                'location'       : geolocation, 
+                'location'       : geolocation,
                 'num_of_reivews' :  num_of_reviews
             }
-            for att in page_attributes:
-                resturant[att] = '1'    
+            for att in page_attributes:  # adding  the attributes to the restaurant JSON
+                resturant[att] = '1'
             print(resturant)
-            data.append(resturant)
-        except Exception as e:
+            data_local.append(resturant)
+        except Exception as e:  # Some error in extrakting data
             print("[extract_page_attributes] error: ", e)
-    return data
+    return data_local
 
 def get_data_for_pages(num):
     page = get_page_soup("https://www.rest.co.il/restaurants/israel")
-    data.extend(extract_page_attributes(page))
+    data.extend(extract_page_attributes(page))  # adding the page lst of JSON to global data DF
     if(num == 1):
         return data
     for i in range(1,num):
@@ -167,9 +192,44 @@ def get_data_for_pages(num):
         data.extend(extract_page_attributes(page))
     return data
 
+def score_equation(df, row):
+    #ToDO: delete this function or fix to implment instead of labda
+    """
+    calculate score of rest
+    x - c*(s/sqrt(n))
+    x = stars
+    c = 1.96
+    n = number of reviews
+    :param df:
+    :return:
 
+    x̄ = sample mean
+    μ0 = population mean
+    s = sample standard deviation
+    n = sample size
 
-data = get_data_for_pages(1)
-df = pd.DataFrame.from_records(data)
-print(df)
-save_df_to_csv(df)
+    """
+    x = df['stars'][row]
+    c = 1.96
+    n = df['num_of_reviews'][row]
+    s = 1  # std of the stars
+
+    return (x-(c * ( s / math.sqrt(n))))
+
+def load_csv(file_name): #for testing
+    return pd.read_csv(file_name, header=0, sep=',')
+
+def heat_map(df):
+    corr = data.corr()  # heat map
+    sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns)  #
+    plt.show()
+
+# data = get_data_for_pages(1)
+# df = pd.DataFrame.from_records(data)
+# save_df_to_csv(df)
+
+df = load_csv("Resturants Output\Rest df 07.May.2022 12-57-04.csv")
+
+df['score'] = df.apply(lambda row: row.stars - (1.96 * ( 1 / math.sqrt(row.num_of_reivews))), axis=1)  # update score column
+df['score_normalized '] = df.apply(lambda row:  (row.score-min(df.score))/(max(df.score)-min(df.score)), axis=1)  # normalized = (x-min(x))/(max(x)-min(x))
+
